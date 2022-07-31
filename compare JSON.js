a14a544ca7b89a46e09e5711e02a7cdd12a1c7a6
VenkateshDhanakodi <110087599+VenkateshDhanakodi@users.noreply.github.com>
2022-07-31 23:06:40 +0530

    Delete compare JSON.js

--- compare JSON.js
+++
@@ -1,8 +0,0 @@
-var obj1 = {name:"person1", age:5}
-var obj2 = {age : 5, name : "person1"}
-console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
-// let a = JSON.stringify(obj1);
-// let b = JSON.stringify(obj2);
-// console.log(a, b);
-//console.log(_.isEqual(obj1, obj2));
-
